0acbb26ef6e8dd30e1b6711f7fa00433bc677007
lancour <lancour@bu.edu>
2017-11-02 17:24:16 -0400

    Improved memory/speed

--- R/create_kernel.R
+++ R/create_kernel.R
@@ -19,14 +19,20 @@
 #' kernel = CreateKernel(adj.matrix,autosave=TRUE)    #if using autosave
 #' @export
 CreateKernel <- function(adj.mat,lambda=0.1,autosave=FALSE){
-  laplace.mat = -1 * (adj.mat)
+  laplace.mat = -1 * abs(adj.mat)
   for (i in 1:nrow(laplace.mat)){
+    laplace.mat[i,i] = 0
     laplace.mat[i,i] = -1 * sum(laplace.mat[i,])
   }
   id.mat = diag(nrow(laplace.mat))
   adjust.mat = lambda * laplace.mat
+  rm(laplace.mat)
   adjust.mat = adjust.mat + id.mat
-  ker.mat = solve(adjust.mat)
+  rm(id.mat)
+  chol.mat = chol(adjust.mat)
+  rm(adjust.mat)
+  ker.mat = chol2inv(chol.mat)
+  rm(chol.mat)
   rownames(ker.mat) = rownames(adj.mat)
   colnames(ker.mat) = colnames(adj.mat)
   if (autosave==TRUE){
